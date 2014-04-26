3e33d7a94182068bd11da3976918856d7c169fe7
Ashley Davis (SgtCoDFish) <ashley_davis10419@hotmail.com>
2014-04-26 03:11:00 +0100

    Initial commit of project files

---
+++ android/src/com/sgtcodfish/mobiusListing/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.sgtcodfish.mobiusListing.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.sgtcodfish.mobiusListing.MobiusListingGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MobiusListingGame(), config);
+	}
+}
